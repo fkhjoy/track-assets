167f59879a3bce8ee6124a90e113784cf3984304
fkhjoy <khjoyfoysal@gmail.com>
2022-11-13 21:57:26 +0600

    feat: api-documentation ui using swagger

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Track Assets API",
+        default_version="v1",
+        description="APIs for tracking assets",
+        contact=openapi.Contact(email="khjoyfoysal@gmail.com"),
+
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include("accounts.urls")),
     path('api/v1/assets/', include("assets.urls")),
+
+    path('api-documentation/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='api_schema_ui'),
 ]
